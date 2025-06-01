062d2036a310614fbba5ebc50d6cef18543e9460
SprngVel <kraevvalentin98@gmail.com>
2025-06-01 10:51:37 +1000

    refactor:added main structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,123 @@
-// import { useState } from 'react'
+import { createContext, useState } from "react";
+
+// CSS
 import "../src/index.css";
-import Header from "./components/DOM/Header";
-import MultiStepProgressBar from "./components/MultiStepProgressBar";
-import GeneralFormSection from "./components/CV/FormGeneral";
-import { FaArrowRightLong } from "react-icons/fa6";
+
+// REACT ICONS
+import { LuView } from "react-icons/lu";
+import { CiEraser } from "react-icons/ci";
+import { VscFilePdf, VscGitPullRequestGoToChanges } from "react-icons/vsc";
+import { IconContext } from "react-icons";
+
+// COMPONENTS
+import Header from "./components/UI/Header/Header";
+import Section from "./components/Section/Section";
+import AddendumSection from "./components/Section/AddendumSection";
+
+// DATA
+import dataHandler from "./data/initialData";
+
+export const AppDataContext = createContext();
 
 function App() {
+    const [appData, setAppData] = useState({
+        general: [dataHandler.getData("general")],
+        education: [dataHandler.getData("education")],
+        work: [dataHandler.getData("work")],
+        skills: [dataHandler.getData("skills")],
+    });
+
+    function prefillCV() {
+        setAppData({
+            general: [dataHandler.getData("general", true)],
+            education: [dataHandler.getData("education", true)],
+            work: [dataHandler.getData("work", true)],
+            skills: [dataHandler.getData("skills", true)],
+        });
+    }
+
+    function clearCv() {
+        setAppData({
+            general: [...appData.general],
+            education: [],
+            work: [],
+            skills: [{ ...appData.skills, inputs: [] }],
+        });
+    }
+
+    function handleAddSection(sectionName) {
+        setAppData(prevAppData => {
+            return {
+                ...prevAppData,
+                [sectionName]: [...prevAppData[sectionName], dataHandler.getData(sectionName)],
+            };
+        });
+    }
+    // Using carrying (to learn), but making callback and pass it further would be more practical
+    function handleDeleteSection(sectionName) {
+        return id => {
+            setAppData(prevAppData => {
+                const filtered = prevAppData[sectionName].filter(toDeleteObj => toDeleteObj.id !== id);
+                return {
+                    ...prevAppData,
+                    [sectionName]: filtered,
+                };
+            });
+        };
+    }
+
     return (
         <>
             <Header />
-            <MultiStepProgressBar />
             <main>
-                <GeneralFormSection />
-                <button type="button" className="next-step__btn">
-                    <span>Next step</span> <FaArrowRightLong viewBox="0 0 500 512" className="arrow" />
-                </button>
+                <IconContext.Provider value={{ color: "#fff", size: "1.5em", display: "inline-block" }}>
+                    <aside>
+                        <button type="button" aria-label="View">
+                            <LuView />
+                            <span>View</span>
+                        </button>
+                        <button type="button" aria-label="Clear CV" onClick={clearCv}>
+                            <CiEraser />
+                            <span>Clear</span>
+                        </button>
+                        <button type="button" aria-label="Autofill CV" onClick={prefillCV}>
+                            <VscGitPullRequestGoToChanges />
+                            <span>Prefill</span>
+                        </button>
+                        {/*Кнопка должна не рендериться если окно === tablet или монитор*/}
+                        <button type="button" aria-label="PDF">
+                            <VscFilePdf />
+                            <span>PDF</span>
+                        </button>
+                    </aside>
+                </IconContext.Provider>
+                <Section
+                    name="General Information"
+                    step={1}
+                    initialData={appData.general}
+                    onAddSection={() => handleAddSection("general")}
+                    onDelete={handleDeleteSection("general")}
+                />
+                <Section
+                    name="Educational Experience"
+                    step={2}
+                    initialData={appData.education}
+                    onAddSection={() => handleAddSection("education")}
+                    onDelete={handleDeleteSection("education")}
+                />
+                <Section
+                    name="Work Experience"
+                    step={3}
+                    initialData={appData.work}
+                    onAddSection={() => handleAddSection("work")}
+                    onDelete={handleDeleteSection("work")}
+                />
+                <AppDataContext.Provider value={{ appData, setAppData }}>
+                    <AddendumSection
+                        name="Addendum"
+                        step={4}
+                    />
+                </AppDataContext.Provider>
             </main>
         </>
     );
